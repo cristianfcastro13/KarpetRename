cc20db521f175081d911a6576a389967693a625e
Cristian De Santis Castro <mastercastroyt@gmail.com>
2025-09-12 01:33:31 -0500

    Some light refactoring

--- gui_main.py
+++ gui_main.py
@@ -6,17 +6,12 @@ from main import rename_files
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     initial_directory = None
+
+    # If the program was opened with a right-click context menu as 'Advanced Karpet Rename'
     if len(sys.argv) > 1:
         initial_directory = sys.argv[1]
 
-
-        # TODO: Remove these debugging lines later
-        # QMessageBox.information(None, "Info", f"The first argument is: {sys.argv[1]}")
-        # if len(sys.argv) > 2:
-        #     QMessageBox.information(None, "Info", f"The second argument is: {sys.argv[2]}") #Debugging line to show the second argument passed in from the context menu
-        # if len(sys.argv) > 3:
-        #     QMessageBox.information(None, "Info", f"The third argument is: {sys.argv[3]}")
-
+    # If the program was opened with a right-click context menu as 'Simple Karpet Rename'
     if (len(sys.argv) > 2):
         if (sys.argv[2] == "fast"):
             # Call the rename_files function directly with default prefix and suffix
